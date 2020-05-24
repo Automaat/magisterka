6c65926d6959c04ac210d594d2fbed440c23bdee
Marcin Skalski <marcin.skalski@allegro.pl>
2020-05-24 20:09:17 +0200

    fixed paths in build scripts

--- build_all.sh
+++ build_all.sh
@@ -2,10 +2,10 @@ pushd cpu-benchmark
 ./build_and_push_image.sh
 popd
 
-pushd /io-benchmark
+pushd io-benchmark
 ./build_and_push_image.sh
 popd
 
-pushd /load-tests
+pushd load-tests
 ./build_and_push_image.sh
 popd
