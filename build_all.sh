74f9913a72bf5e210153a31629b5da6f19ed8dc7
Marcin Skalski <marcin.skalski@allegro.pl>
2020-05-24 19:56:32 +0200

    changed gradle user

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,11 @@
-pushd $TRAVIS_BUILD_DIR/cpu-benchmark
+pushd cpu-benchmark
 ./build_and_push_image.sh
 popd
 
-pushd $TRAVIS_BUILD_DIR/io-benchmark
+pushd /io-benchmark
 ./build_and_push_image.sh
 popd
 
-pushd $TRAVIS_BUILD_DIR/load-tests
+pushd /load-tests
 ./build_and_push_image.sh
 popd
--- cpu-benchmark/build_and_push_image.sh
+++ cpu-benchmark/build_and_push_image.sh
@@ -1,3 +1,3 @@
-docker run -u gradle -v $(pwd):/home/gradle/project -w /home/gradle/project gradle gradle --no-daemon clean jmhJar
+docker run -v $(pwd):/home/gradle/project -w /home/gradle/project gradle gradle --no-daemon clean jmhJar
 docker build -t automaat/cpu-benchmark .
 docker push automaat/cpu-benchmark
