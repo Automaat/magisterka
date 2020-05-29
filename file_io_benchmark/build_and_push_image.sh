6c0801052911c6961fa64ccc20ce7d9669461cd9
Marcin Skalski <marcin.skalski@allegro.pl>
2020-05-29 09:50:53 +0200

    updated benchmarking configuration

--- file_io_benchmark/build_and_push_image.sh
+++ file_io_benchmark/build_and_push_image.sh
@@ -1,3 +1,3 @@
 docker run -v $(pwd):/home/gradle/project -w /home/gradle/project gradle gradle --no-daemon clean jmhJar
 docker build -t automaat/file_io_benchmark .
-#docker push automaat/file_io_benchmark
+docker push automaat/file_io_benchmark
